f1762a3e86f565b75f2e675382c966fb372eae41
priyaihsan <priyaihsan771@gmail.com>
2021-04-10 06:36:05 +0700

    first commit

---
+++ praktikum_jquery.js
@@ -0,0 +1,47 @@
+document.getElementById('nama').innerHTML = 'priya ihsan';
+
+
+$('#nim').html('20190140050');
+
+$(function(){
+    console.log("hello");
+})
+
+$("#hide").on("click",function(){
+    $("#nama").hide();
+});
+
+
+$("#show").on("click",function(){
+    $("#nama").show();
+});
+
+$("#toggle").on("click",function(){
+    $("#nama").toggle();
+});
+
+$("fadeIn").on("click",function(){
+    $("#tanggal").fadeIn();
+});
+
+$("#fadeOut").on("click",function(){
+    $("#tanggal").fadeOut();
+});
+
+$("#fadeToggle").on("click",function(){
+    $("#tanggal").fadeToggle();
+});
+
+$("#slideUp").on("click",function(){
+    $("#img").slideUp();
+});
+
+$("#slideDown").on("click",function(){
+    $("#img").slideDown();
+});
+
+$("#slideToggle").on("click",function(){
+    $("#img").slideToggle();
+});
+
+
